4d3b9a9f9d207d3c1152675dda69fae3132b3242
andriko19 <andrik.suprayitno@gmail.com>
2025-03-07 16:29:20 +0700

    init project template auth laravel 8

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//INDEX
+Route::get('/', [LoginController::class, 'index'])->middleware('guest');
+
+//AUTH MANUAL
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login')->middleware('guest');
+Route::match(['get','post'],'/logout', [LoginController::class, 'logout'])->name('logout');
+
+//DASHBOARD
+Route::resource('dashboard', 'DashboardController')->except(['show'])->middleware('auth');
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
